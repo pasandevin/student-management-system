9231c68a4d91cb53001dbbf6ecbba659557dd5bb
pasandevin <jayawardene.pasandevin@gmail.com>
2022-03-09 21:38:43 +0530

    database connected

---
+++ backend/src/main/java/com/kelaniya/uni/backend/Student.java
@@ -0,0 +1,10 @@
+package com.kelaniya.uni.backend;
+
+public class Student {
+
+    public String id;
+    public String name;
+    public int age;
+    public String city;
+
+}
--- backend/src/main/java/com/kelaniya/uni/backend/StudentController.java
+++ backend/src/main/java/com/kelaniya/uni/backend/StudentController.java
@@ -1,14 +1,25 @@
 package com.kelaniya.uni.backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
 
+    @Autowired
+    StudentRepository studentRepository;
+
     @GetMapping("/student")
     public String getStudent() {
         return "Pasan, 17, Horana";
+    }
+
+    @PostMapping("/student")
+    public void addStudent(@RequestBody Student studentData) {
+        studentRepository.save(studentData);
     }
 
 }
